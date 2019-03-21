32bcc45461317425670d87d48970d6f18e6ae903
topetin <carballalmariana@gmail.com>
2019-03-21 17:03:48 -0300

    Handlevars and templates

--- src/app.js
+++ src/app.js
@@ -2,11 +2,33 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+//last insert
 app.use(express.static(publicDirectoryPath))
+
 app.get('', (req, res) => {
-    res.send('hello express')
+    res.render('index', {
+        title: 'Weather',
+        name: 'Mariana'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About page'
+    }) 
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page'
+    })
 })
 
 app.listen(3000, () => {
