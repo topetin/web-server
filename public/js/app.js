0440ca6dba391232844b47bc49d653150bb30593
topetin <carballalmariana@gmail.com>
2019-03-22 13:58:09 -0300

    form in index to search weather making client side http request

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
-console.log('runninggg');
+const weatherForm = document.querySelector('#weather')
+const searchElement = document.querySelector('#location-input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = searchElement.value
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+         data.error ? console.log(data.error) : console.log(data.location)
+    })
+})
+})
